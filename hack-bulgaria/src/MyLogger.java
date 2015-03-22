f8778a276cecc4ebe5a52fc1dc6d9fddeb8b29d4
eek <eek@ss1>
2015-03-22 18:01:55 +0200

    HTTP Post implemented

--- hack-bulgaria/src/MyLogger.java
+++ hack-bulgaria/src/MyLogger.java
@@ -26,16 +26,22 @@ The FileLogger should log the messages to a given file.
 HTTPLogger
 The HTTPLogger shoud log the messages via a POST request to a given HTTP url.
 */
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 interface MyLogger {
-	public void log(int level, String message);
+	public void log(int level, String message) throws MalformedURLException, IOException;
 }
 
 class ConsoleLogger implements MyLogger {
@@ -92,9 +98,18 @@ class FileLogger implements MyLogger {
 }
 
 class HTTPLogger implements MyLogger {
-
+	public static String user_agent = "Mozilla/5.0";
+	
+	public void printLog(int level, String message) throws IOException {
+		HTTPLogger http = new HTTPLogger();
+		
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.log(level, message);
+	}
+	
 	@Override
-	public void log(int level, String message) {
+	public void log(int level, String message) throws IOException {
+		
 		String levelStr, log;
 		if (level<1 || level>3)
 			System.err.println("Error, valid level values are 1,2 and 3.");
@@ -105,10 +120,46 @@ class HTTPLogger implements MyLogger {
 			case(2): levelStr="WARNING"; break;
 			case(3): levelStr="PLSCHECKFFS"; break;
 			}
-			// In ISO 8601, as ISO 8901 does not exist.
 			Instant time = Instant.now();
 			log = levelStr + "::" + time.truncatedTo(ChronoUnit.SECONDS).toString() + "::" + message;
 			//TODO POST transaction, unfortunately out of my scope of knowledge :)
+			String url = "https://selfsolve.apple.com/wcResults.do";
+			
+			URL obj= new URL(url);
+
+				HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			//add request header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", user_agent);
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+			
+			String urlParameters = "sn="+log;
+			
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+			
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + urlParameters);
+			System.out.println("Response Code : " + responseCode);
+	 
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+	 
+			//print result
+			System.out.println(response.toString());
+	 
 		}
 	}
 	
--- hack-bulgaria/src/MyLoggerTester.java
+++ hack-bulgaria/src/MyLoggerTester.java
@@ -1,13 +1,17 @@
+import java.io.IOException;
+
 /*
  * Tester class for the Logger classes in MyLogger.java
  */
 public class MyLoggerTester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ConsoleLogger cl = new ConsoleLogger();
 		cl.log(1, "Hello");
 		FileLogger fl = new FileLogger();
 		fl.log(2, "World");
+		HTTPLogger hl = new HTTPLogger();
+		hl.printLog(3, "Half-Life");
 	}
 
 }
